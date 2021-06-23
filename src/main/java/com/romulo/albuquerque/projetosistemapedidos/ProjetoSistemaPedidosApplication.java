448019b86a2c647dcd0fa13ddf22b2080050e9bf
Romulo Albuquerque <romulo.albuquerque@live.com>
2021-06-23 19:47:07 -0300

    Created project

---
+++ src/main/java/com/romulo/albuquerque/projetosistemapedidos/ProjetoSistemaPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.romulo.albuquerque.projetosistemapedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSistemaPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSistemaPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romulo/albuquerque/projetosistemapedidos/ProjetoSistemaPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romulo.albuquerque.projetosistemapedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSistemaPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
