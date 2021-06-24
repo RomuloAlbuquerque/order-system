9612aa4dff66e368364703956dda888ef2b4f252
Romulo Albuquerque <romulo.albuquerque@live.com>
2021-06-23 22:53:44 -0300

    H2 database and automatic database creation

--- src/test/java/com/romulo/albuquerque/ordersystem/ProjetoSistemaPedidosApplicationTests.java
+++ src/test/java/com/romulo/albuquerque/ordersystem/ProjetoSistemaPedidosApplicationTests.java
@@ -1,4 +1,4 @@
-package com.romulo.albuquerque.projetosistemapedidos;
+package com.romulo.albuquerque.ordersystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
